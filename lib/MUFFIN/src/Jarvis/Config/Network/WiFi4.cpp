d7b116086534640d8c1304cd4f15f3f6c111fcfd
Lee, Sang-Jin <lsj31@vitcon.co.kr>
2024-09-04 11:23:43 +0900

    WiFi4 클래스 개발 및 테스트 완료
    WiFi4 클래스와 함께 개발한 클래스
    - muffin::jarvis::config::WiFi4 클래스
    
    개발 및 테스트 완료 기능
    - muffin::jarvis::config::WiFi4 클래스 생성, 소멸, 연산자 오버로딩, getter/setter
    - muffin::WiFi4 클래스 생성, 소멸, 설정, 연결, 연결 끊기, 재연결, 상태 관련 정보 getter
    
    향후 개발사항
    - EAP가 적용된 기업용 Wi-Fi 망에 연결하기 위해서는 WPA2 Auth Mode 정보를 사용자로부터 추가로 입력받아야 함
    - 현재는 Wi-Fi 클라이언트 모드만 개발하였으나 향후 AP 모드 또는 동시에 동작시키기 위해서는 추가 개발이 필요함
    - AP 모드 개발 시 개발 문서 상 최대 클라이언트 개수인 15개를 가져갈지 아니면 메모리를 고려해 그보다 적게 연결을 받을지를 정해야 함

---
+++ lib/MUFFIN/src/Jarvis/Config/Network/WiFi4.cpp
@@ -0,0 +1,331 @@
+/**
+ * @file WiFi4.cpp
+ * @author Lee, Sang-jin (lsj31@edgecross.ai)
+ * 
+ * @brief Wi-Fi 인터페이스 설정 정보를 관리하는 클래스를 정의합니다.
+ * 
+ * @date 2024-09-03
+ * @version 0.0.1
+ * 
+ * @copyright Copyright Edgecross Inc. (c) 2024
+ */
+
+
+
+
+#include "Logger/Logger.h"
+#include "WiFi4.h"
+
+
+
+namespace muffin { namespace jarvis { namespace config {
+
+    WiFi4::WiFi4()
+        : Base("wifi")
+    {
+        LOG_DEBUG(logger, "Constructed at address: %p", this);
+    }
+
+    WiFi4::~WiFi4()
+    {
+        LOG_DEBUG(logger, "Destroyed at address: %p", this);
+    }
+
+    void WiFi4::operator=(const WiFi4& obj)
+    {
+        mEnableDHCP   = obj.mEnableDHCP;
+        mStaticIPv4   = obj.mStaticIPv4;
+        mSubnetmask   = obj.mSubnetmask;
+        mGateway      = obj.mGateway;
+        mDNS1         = obj.mDNS1;
+        mDNS2         = obj.mDNS2;
+        mSSID         = obj.mSSID;
+        mPSK          = obj.mPSK;
+        mAuthMode     = obj.mAuthMode;
+        mEnableEAP    = obj.mEnableEAP;
+        mEapID        = obj.mEapID;
+        mEapUserName  = obj.mEapUserName;
+        mEapPassword  = obj.mEapPassword;
+    }
+
+    bool WiFi4::operator==(const WiFi4& obj) const
+    {
+        return (
+            mEnableDHCP   == obj.mEnableDHCP  &&
+            mStaticIPv4   == obj.mStaticIPv4  &&
+            mSubnetmask   == obj.mSubnetmask  &&
+            mGateway      == obj.mGateway     &&
+            mDNS1         == obj.mDNS1        &&
+            mDNS2         == obj.mDNS2        &&
+            mSSID         == obj.mSSID        &&
+            mPSK          == obj.mPSK         &&
+            mAuthMode     == obj.mAuthMode    &&
+            mEnableEAP    == obj.mEnableEAP   &&
+            mEapID        == obj.mEapID       &&
+            mEapUserName  == obj.mEapUserName &&
+            mEapPassword  == obj.mEapPassword
+        );
+    }
+
+    bool WiFi4::operator!=(const WiFi4& obj) const
+    {
+        return !(*this == obj);
+    }
+
+    Status WiFi4::SetDHCP(const bool enableDHCP)
+    {
+        mEnableDHCP = enableDHCP;
+        if (mEnableDHCP == enableDHCP)
+        {
+            return Status(Status::Code::GOOD_ENTRY_REPLACED);
+        }
+        else
+        {
+            return Status(Status::Code::BAD_DEVICE_FAILURE);
+        }
+    }
+
+    Status WiFi4::SetStaticIPv4(const std::string& staticIPv4)
+    {
+        if (mStaticIPv4.fromString(staticIPv4.c_str()))
+        {
+            return Status(Status::Code::GOOD_ENTRY_REPLACED);
+        }
+        else
+        {
+            return Status(Status::Code::BAD_INVALID_ARGUMENT);
+        }
+    }
+
+    Status WiFi4::SetSubnet(const std::string& subnetmask)
+    {
+        if (mSubnetmask.fromString(subnetmask.c_str()))
+        {
+            return Status(Status::Code::GOOD_ENTRY_REPLACED);
+        }
+        else
+        {
+            return Status(Status::Code::BAD_INVALID_ARGUMENT);
+        }
+    }
+
+    Status WiFi4::SetGateway(const std::string& gateway)
+    {
+        if (mGateway.fromString(gateway.c_str()))
+        {
+            return Status(Status::Code::GOOD_ENTRY_REPLACED);
+        }
+        else
+        {
+            return Status(Status::Code::BAD_INVALID_ARGUMENT);
+        }
+    }
+
+    Status WiFi4::SetDNS1(const std::string& dns1)
+    {
+        if (mDNS1.fromString(dns1.c_str()))
+        {
+            return Status(Status::Code::GOOD_ENTRY_REPLACED);
+        }
+        else
+        {
+            return Status(Status::Code::BAD_INVALID_ARGUMENT);
+        }
+    }
+
+    Status WiFi4::SetDNS2(const std::string& dns2)
+    {
+        if (mDNS2.fromString(dns2.c_str()))
+        {
+            return Status(Status::Code::GOOD_ENTRY_REPLACED);
+        }
+        else
+        {
+            return Status(Status::Code::BAD_INVALID_ARGUMENT);
+        }
+    }
+
+    Status WiFi4::SetSSID(const std::string& ssid)
+    {
+        assert(ssid.length() < 33);
+
+        mSSID = ssid;
+        if (mSSID == ssid)
+        {
+            return Status(Status::Code::GOOD_ENTRY_REPLACED);
+        }
+        else
+        {
+            return Status(Status::Code::BAD_DEVICE_FAILURE);
+        }
+    }
+
+    Status WiFi4::SetPSK(const std::string& psk)
+    {
+        assert(psk.length() < 65);
+
+        mPSK = psk;
+        if (mPSK == psk)
+        {
+            return Status(Status::Code::GOOD_ENTRY_REPLACED);
+        }
+        else
+        {
+            return Status(Status::Code::BAD_DEVICE_FAILURE);
+        }
+    }
+
+    Status WiFi4::SetAuthMode(const std::string& auth)
+    {
+        assert(auth == "open" || auth == "wep" || auth == "wpa_psk" || auth == "wpa2_psk" || auth == "wpa_wpa2_psk" || auth == "wpa2_enterprise");
+
+        if (auth == "open")
+        {
+            mAuthMode = wifi_auth_mode_t::WIFI_AUTH_OPEN;
+        }
+        else if (auth == "wep")
+        {
+            mAuthMode = wifi_auth_mode_t::WIFI_AUTH_WEP;
+        }
+        else if (auth == "wpa_psk")
+        {
+            mAuthMode = wifi_auth_mode_t::WIFI_AUTH_WPA_PSK;
+        }
+        else if (auth == "wpa2_psk")
+        {
+            mAuthMode = wifi_auth_mode_t::WIFI_AUTH_WPA2_PSK;
+        }
+        else if (auth == "wpa_wpa2_psk")
+        {
+            mAuthMode = wifi_auth_mode_t::WIFI_AUTH_WPA_WPA2_PSK;
+        }
+        else
+        {
+            mAuthMode = wifi_auth_mode_t::WIFI_AUTH_WPA2_ENTERPRISE;
+        }
+        return Status(Status::Code::GOOD);
+    }
+
+    Status WiFi4::SetEAP(const bool& eap)
+    {
+        mEnableEAP = eap;
+        if (mEnableEAP == eap)
+        {
+            return Status(Status::Code::GOOD_ENTRY_REPLACED);
+        }
+        else
+        {
+            return Status(Status::Code::BAD_DEVICE_FAILURE);
+        }
+    }
+
+    Status WiFi4::SetEapID(const std::string& eapID)
+    {
+        assert(eapID.length() < 65);
+
+        mEapID = eapID;
+        if (mEapID == eapID)
+        {
+            return Status(Status::Code::GOOD_ENTRY_REPLACED);
+        }
+        else
+        {
+            return Status(Status::Code::BAD_DEVICE_FAILURE);
+        }
+    }
+
+    Status WiFi4::SetEapUserName(const std::string& eapUserName)
+    {
+        assert(eapUserName.length() < 65);
+
+        mEapUserName = eapUserName;
+        if (mEapUserName == eapUserName)
+        {
+            return Status(Status::Code::GOOD_ENTRY_REPLACED);
+        }
+        else
+        {
+            return Status(Status::Code::BAD_DEVICE_FAILURE);
+        }
+    }
+
+    Status WiFi4::SetEapPassword(const std::string& eapPassword)
+    {
+        assert(eapPassword.length() < 65);
+
+        mEapPassword = eapPassword;
+        if (mEapPassword == eapPassword)
+        {
+            return Status(Status::Code::GOOD_ENTRY_REPLACED);
+        }
+        else
+        {
+            return Status(Status::Code::BAD_DEVICE_FAILURE);
+        }
+    }
+
+    bool WiFi4::GetDHCP() const
+    {
+        return mEnableDHCP;
+    }
+
+    const IPAddress& WiFi4::GetStaticIPv4() const
+    {
+        return mStaticIPv4;
+    }
+
+    const IPAddress& WiFi4::GetSubnet() const
+    {
+        return mSubnetmask;
+    }
+
+    const IPAddress& WiFi4::GetGateway() const
+    {
+        return mGateway;
+    }
+
+    const IPAddress& WiFi4::GetDNS1() const
+    {
+        return mDNS1;
+    }
+
+    const IPAddress& WiFi4::GetDNS2() const
+    {
+        return mDNS2;
+    }
+
+    const std::string& WiFi4::GetSSID() const
+    {
+        return mSSID;
+    }
+
+    const std::string& WiFi4::GetPSK() const
+    {
+        return mPSK;
+    }
+
+    wifi_auth_mode_t WiFi4::GetAuthMode() const
+    {
+        return mAuthMode;
+    }
+
+    bool WiFi4::GetEAP() const
+    {
+        return mEnableEAP;
+    }
+
+    const std::string& WiFi4::GetEapID() const
+    {
+        return mEapID;
+    }
+
+    const std::string& WiFi4::GetEapUserName() const
+    {
+        return mEapUserName;
+    }
+
+    const std::string& WiFi4::GetEapPassword() const
+    {
+        return mEapPassword;
+    }
+}}}
---
+++ lib/MUFFIN/src/Jarvis/Config/Network/WiFi4.h
@@ -0,0 +1,85 @@
+/**
+ * @file WiFi4.h
+ * @author Lee, Sang-jin (lsj31@edgecross.ai)
+ * 
+ * @brief Wi-Fi 인터페이스 설정 정보를 관리하는 클래스를 선언합니다.
+ * 
+ * @date 2024-09-03
+ * @version 0.0.1
+ * 
+ * @todo JARVIS 설계 문서에 WPA2 authentication method 속성을 추가로 정의해야 합니다.
+ * 그러나 그에 앞서서 각 auth 모드에 따라 사용자로부터 추가로 입력을 받아야 하는 데이터가 
+ * 어떤 게 있는지를 먼저 확인해야 합니다. WPA2 auth 방법에는 아래의 세 가지가 있습니다.
+ *   - WPA2_AUTH_TLS, WPA2_AUTH_PEAP, WPA2_AUTH_TTLS
+ * 
+ * @copyright Copyright Edgecross Inc. (c) 2024
+ */
+
+
+
+
+#pragma once
+
+#include <esp_wifi_types.h>
+#include <IPAddress.h>
+
+#include "Common/Status.h"
+#include "Jarvis/Config/Base.h"
+
+
+
+namespace muffin { namespace jarvis { namespace config {
+
+    class WiFi4 : public Base
+    {
+    public:
+        WiFi4();
+        virtual ~WiFi4() override;
+    public:
+        void operator=(const WiFi4& obj);
+        bool operator==(const WiFi4& obj) const;
+        bool operator!=(const WiFi4& obj) const;
+    public:
+        Status SetDHCP(const bool enableDHCP);
+        Status SetStaticIPv4(const std::string& staticIPv4);
+        Status SetSubnet(const std::string& subnetmask);
+        Status SetGateway(const std::string& gateway);
+        Status SetDNS1(const std::string& dns1);
+        Status SetDNS2(const std::string& dns2);
+        Status SetSSID(const std::string& ssid);
+        Status SetPSK(const std::string& psk) ;
+        Status SetAuthMode(const std::string& auth);
+        Status SetEAP(const bool& eap);
+        Status SetEapID(const std::string& eapID);
+        Status SetEapUserName(const std::string& eapUserName);
+        Status SetEapPassword(const std::string& eapPassword);
+    public:
+        bool GetDHCP() const;
+        const IPAddress& GetStaticIPv4() const;
+        const IPAddress& GetSubnet() const;
+        const IPAddress& GetGateway() const;
+        const IPAddress& GetDNS1() const;
+        const IPAddress& GetDNS2() const;
+        const std::string& GetSSID() const;
+        const std::string& GetPSK() const;
+        wifi_auth_mode_t GetAuthMode() const;
+        bool GetEAP() const;
+        const std::string& GetEapID() const;
+        const std::string& GetEapUserName() const;
+        const std::string& GetEapPassword() const;
+    private:
+        bool mEnableDHCP = true;
+        IPAddress mStaticIPv4;
+        IPAddress mSubnetmask;
+        IPAddress mGateway;
+        IPAddress mDNS1;
+        IPAddress mDNS2;
+        std::string mSSID;
+        std::string mPSK;
+        wifi_auth_mode_t mAuthMode;
+        bool mEnableEAP = false;
+        std::string mEapID;
+        std::string mEapUserName;
+        std::string mEapPassword;
+    };    
+}}}
