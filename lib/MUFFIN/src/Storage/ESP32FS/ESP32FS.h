367924c8fed93b7b72dc726f0295bedba5641a9f
Lee, Sang-Jin <lsj31@vitcon.co.kr>
2024-09-05 14:58:57 +0900

    ESP32FS 클래스 개발 및 테스트 완료
    ESP32FS 클래스와 함께 개발한 클래스
    - muffin::IStorage 클래스
    
    개발 및 테스트 완료 기능
    - MODLINK에서 사용 가능한 모든 Storage 클래스의 공통 인터페이스 작성
    - muffin::ESP32FS 클래스 생성, 소멸, (u)mount, 파일 조회/생성/삭제/이름변경, 상태 관련 정보 getter
    
    향후 개발사항
    - Arduino 프레임워크의 제약으로 인해 특정 기능이 실패했을 때 원인을 찾을 수 없음
    - 따라서 향후 ESP-IDF 프레임워크로 이전 시 기능에 대한 로우 레벨의 개발이 필요함

---
+++ lib/MUFFIN/src/Storage/ESP32FS/ESP32FS.h
@@ -0,0 +1,52 @@
+/**
+ * @file ESP32FS.h
+ * @author Lee, Sang-jin (lsj31@edgecross.ai)
+ * 
+ * @brief ESP32 LittleFS 파일 시스템을 사용하는데 필요한 기능을 제공하는 클래스를 선언합니다.
+ * 
+ * @date 2024-09-05
+ * @version 0.0.1
+ * 
+ * @note Arduino 프레임워크의 제약으로 인해 End() 실행 결과를 확인할 수 있는 방법이 없음 
+ * @todo ESP-IDF 프레임워크로 이전 시 파티션 unregister, unmount 결과 값에 따라 실행 결과를 리턴하게 수정해야 함
+ * 
+ * @copyright Copyright Edgecross Inc. (c) 2024
+ */
+
+
+
+
+#pragma once
+
+#include "Storage/IStorage.h"
+
+
+
+namespace muffin {
+
+    class ESP32FS : public IStorage
+    {
+    public:
+        ESP32FS();
+        virtual ~ESP32FS() override;
+    public:
+        virtual Status Begin(const bool formatOnFail = false, const char* basePath = "/littlefs", const uint8_t maxOpenFiles = 10, const char* partitionLabel = "spiffs") override;
+        virtual Status Format() override;
+        virtual size_t GetTotalBytes() const override;
+        virtual size_t GetUsedBytes() const override;
+        virtual Status End() override;
+    public:
+        virtual File Open(const char* path, const char* mode = FILE_READ, const bool create = false) override;
+        virtual File Open(const std::string& path, const char* mode = FILE_READ, const bool create = false) override;
+        virtual Status DoesExist(const char* path) override;
+        virtual Status DoesExist(const std::string& path) override;
+        virtual Status Remove(const char* path) override;
+        virtual Status Remove(const std::string& path) override;
+        virtual Status Rename(const char* pathFrom, const char* pathTo) override;
+        virtual Status Rename(const std::string& pathFrom, const std::string& pathTo) override;
+        virtual Status MakeDirectory(const char* path) override;
+        virtual Status MakeDirectory(const std::string &path) override;
+        virtual Status RemoveDirectory(const char* path) override;
+        virtual Status RemoveDirectory(const std::string &path) override;
+    };
+}
