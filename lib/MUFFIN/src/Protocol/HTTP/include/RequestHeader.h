169cd655ab42f7f58658bc76a852925dfc1b3e33
Lee, Sang-Jin <lsj31@vitcon.co.kr>
2024-09-19 10:06:01 +0900

    CatHTTP 테스트 및 개발 완료
    CatHTTP 클래스와 함께 개발한 클래스
    - muffin::http::RequestHeader 클래스
    - muffin::http::RequestBody 클래스
    - muffin::http::RequestParameter 클래스
    
    개발 및 테스트 완료 기능
    - muffin::http::CatHTTP 클래스 생성, 소멸, 설정 및 상태 관련 정보 getter
    - PDP context 및 SSL context 설정, 기타 HTTP 프로토콜 설정
    - HTTP GET, POST 메서드 요청 및 응답 수신(ESP32 또는 CatFS) 기능
    - 응답은 기본적으로 UART 라인을 사용해 ESP32에 전송
    
    향후 개발사항
    - Init() 서브루틴에 미구현된 checker 함수 구현 및 적용
    - UserAgent 속성 값에 모드링크 모델명과 펌웨어 버전 추가 필요
    - 다른 NIC와 동일한 HTTP 프로토콜 인터페이스를 가지게 수정이 필요
    
    HTTP request parameter 기능 추가
    
    - RequestParameter 클래스 추가
    - JARVIS API 서버로 테스트 오나료

---
+++ lib/MUFFIN/src/Protocol/HTTP/include/RequestHeader.h
@@ -0,0 +1,109 @@
+/**
+ * @file RequestHeader.h
+ * @author Lee, Sang-jin (lsj31@edgecross.ai)
+ * 
+ * @brief HTTP 프로토콜 헤더 정보를 표현하는 클래스를 선언합니다.
+ * 
+ * @date 2024-09-19
+ * @version 0.0.1
+ * 
+ * @note 헤더 설정 시 유효성을 검증하는 절차가 필요하다는 생각으로 모든
+ *       setter 함수가 Status를 반환하도록 시그니처를 작성했습니다. 단,
+ *       현재는 시간 제약으로 인해 구현은 어렵습니다. 향후 버전 개발 시
+ *       본 노트를 참조하면 좋겠습니다.
+ * 
+ * @todo 필요하다고 생각되거나 시간적인 여유가 있다면 setter 함수의 
+ *       매개변수로 들어온 데이터의 유효성을 검증할 수 있는 절차를 
+ *       구현해야 합니다.
+ * 
+ * @todo UserAgent 속성에 들어갈 MODLINK 모델 이름과 펌웨어 버전 정보는
+ *       향후 정보모델 내에서 관리되는 값을 참조하도록 수정되어야 합니다.
+ * 
+ * @copyright Copyright Edgecross Inc. (c) 2024
+ */
+
+
+
+
+#pragma once
+
+#include <bitset>
+#include <string>
+
+#include "Common/Status.h"
+#include "TypeDefinitions.h"
+
+
+
+namespace muffin { namespace http {
+
+    class RequestHeader
+    {
+    public:
+        RequestHeader(const rest_method_e method, const http_scheme_e scheme, const std::string& host, const uint16_t port, const std::string& path, const std::string& userAgent);
+        virtual ~RequestHeader();
+    public:
+        const char* c_str() const;
+        std::string ToString() const;
+        std::string GetURL() const;
+    public:
+        Status SetAccept(const std::string& accept);
+        Status SetAcceptEncoding(const std::string& encoding);
+        Status SetCacheControl(const std::string& cacheControl);
+        Status SetConnection(const std::string& connection);
+        Status SetContentLength(const size_t contentLength);
+        Status SetContentType(const std::string& contentType);
+        Status SetHost(const std::string& host);
+        Status SetPort(const uint16_t port);
+        Status SetUserAgent(const std::string& userAgent);
+        Status SetVersion(const std::string& version);
+        Status UpdateParamter(const std::string& parameter);
+    public:
+        const std::string& GetAccept() const;
+        const std::string& GetAcceptEncoding() const;
+        const std::string& GetCacheControl() const;
+        const std::string& GetConnection() const;
+        size_t GetContentLength() const;
+        const std::string& GetContentType() const;
+        const std::string& GetHost() const;
+        uint16_t GetPort() const;
+        const std::string& GetUserAgent() const;
+        const std::string& GetVersion() const;
+    private:
+        // bool isValidMediaType(const std::string& mediaType);
+        // bool isValidQualityFactor(const std::string& qualityFactor);
+    private:
+        std::string mAccept;
+        std::string mAcceptEncoding;
+        std::string mCacheControl;
+        std::string mConnection;
+        size_t mContentLength;
+        std::string mContentType;
+        std::string mHost;
+        rest_method_e mMethod;
+        std::string mPath;
+        uint16_t mPort;
+        http_scheme_e mScheme;
+        std::string mUserAgent;
+        std::string mVersion;
+    private:
+        typedef enum class HeaderPropertyFlagEnum
+            : uint8_t
+        {
+            ACCEPT            =  0,
+            ACCEPT_ENCODING   =  1,
+            CACHE_CONTROL     =  2,
+            CONNECTION        =  3,
+            CONTENT_LENGTH    =  4,
+            CONTENT_TYPE      =  5,
+            HOST              =  6,
+            METHOD            =  7,
+            PATH              =  8,
+            PORT              =  9,
+            SCHEME            = 10,
+            USER_AGENT        = 11,
+            VERSION           = 12
+        } property_flag_e;
+        std::bitset<13> mPropertyFlags;
+    };
+}}
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,113 @@
 #include <Arduino.h>
+#include <Jarvis/Config/Network/CatM1.h>
+#include <Network/CatM1/CatM1.h>
+#include <Network/Ethernet/Ethernet.h>
+#include <Network/WiFi4/WiFi4.h>
+#include <Network/TypeDefinitions.h>
+#include <Protocol/HTTP/HttpHeader.h>
+#include <Protocol/HTTP/CatHTTP/CatHTTP.h>
+#include <Protocol/MQTT/CatMQTT/CatMQTT.h>
+#include <Storage/ESP32FS/ESP32FS.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <Protocol/HTTP/HttpResponse.h>
+#include <Protocol/HTTP/HttpQueue.h>
+#include <vector>
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+void setup()
+{
+    muffin::logger = new muffin::Logger();
+
+    using namespace muffin;
+    std::vector<uint8_t> buffer = { 0, 1, 2, 3, 4 };
+    HttpResponse<std::vector<uint8_t>> response(buffer);
+
+    HttpQueue queue;
+    LOG_DEBUG(logger, "Occupied: %u", queue.CountOccupied());
+    queue.Send(response);
+    LOG_DEBUG(logger, "Occupied: %u", queue.CountOccupied());
+
+    HttpResponse<uint8_t>* retrievedBuffer = queue.Peek();
+    for (const auto& datum : retrievedBuffer)
+    {
+        Serial.print(datum);
+        Serial.print(" ");
+    }
+    Serial.println();
+    
+
+    // muffin::jarvis::config::CatM1 config;
+    // config.SetCounty("KR");
+    // config.SetModel("LM5");
+
+    // muffin::CatM1 catM1;
+    // catM1.Config(&config);
+    // catM1.Init();
+
+    // while (catM1.GetState() != muffin::CatM1::state_e::SUCCEDDED_TO_START)
+    // {
+    //     vTaskDelay(1000 / portTICK_PERIOD_MS);
+    // }
+
+    // while (catM1.Connect() != muffin::Status::Code::GOOD)
+    // {
+    //     vTaskDelay(1000 / portTICK_PERIOD_MS);
+    // }
+
+    // /***********************************************************/
+
+    // muffin::mqtt::BrokerInfo info("test_id_mac_modlink_l");
+    // muffin::mqtt::CatMQTT mqtt(catM1, info);
+    // mqtt.Init(muffin::network::lte::pdp_ctx_e::PDP_01, muffin::network::lte::ssl_ctx_e::SSL_0);
+    // while (mqtt.Connect() != muffin::Status::Code::GOOD)
+    // {
+    //     vTaskDelay(3000 / portTICK_PERIOD_MS);
+    // }
+
+    // std::vector<muffin::mqtt::Message> vec;
+    // muffin::mqtt::Message message(
+    //     muffin::mqtt::topic_e::JARVIS_READ_PARAMETER,
+    //     ""
+    // );
+    // vec.emplace_back(message);
+    // mqtt.Subscribe(vec);
+
+    // /***********************************************************/
+
+    // muffin::http::CatHTTP http(catM1);
+    // LOG_INFO(muffin::logger, "HTTP Init: %s",
+    //     http.Init(muffin::network::lte::pdp_ctx_e::PDP_01,
+    //               muffin::network::lte::ssl_ctx_e::SSL_0
+    //     ).c_str()
+    // );
+
+    // muffin::http::HttpHeader header1(
+    //     muffin::rest_method_e::GET,
+    //     muffin::http_scheme_e::HTTP,
+    //     "ec2-3-38-208-214.ap-northeast-2.compute.amazonaws.com",
+    //     5000,
+    //     "/api/server/time",
+    //     "MODLINK-L/0.0.1"
+    // );
+    // http.GET(header1);
+
+    // muffin::http::HttpHeader header2(
+    //     muffin::rest_method_e::POST,
+    //     muffin::http_scheme_e::HTTP,
+    //     "ec2-3-38-208-214.ap-northeast-2.compute.amazonaws.com",
+    //     5000,
+    //     "/api/firmware/version",
+    //     "MODLINK-L/0.0.1"
+    // );
+
+    // muffin::http::HttpBody body("application/x-www-form-urlencoded");
+    // body.AddProperty("cp", "1000");
+    // body.AddProperty("md", "TEST");
+    // http.POST(header2, body);
+    // delay(UINT32_MAX);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+    // put your main code here, to run repeatedly:
 }
