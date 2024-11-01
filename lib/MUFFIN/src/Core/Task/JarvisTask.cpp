c104b1abb2916ca6baa9ff020d831bc19caa1773
Lee-SangJin <lsj31@vitcon.co.kr>
2024-11-01 16:35:47 +0900

    Data Unit Order가 1개일 때 엔디언으로 인해 데이터가 잘못 처리되는 버그 수정함

--- lib/MUFFIN/src/Core/Task/JarvisTask.cpp
+++ lib/MUFFIN/src/Core/Task/JarvisTask.cpp
@@ -463,9 +463,9 @@ namespace muffin {
 
     void applyOperationCIN(std::vector<jarvis::config::Base*>& vectorOperationCIN)
     {
-        LOG_DEBUG(logger, "Start applying Operation CIN");
-        jarvis::config::Operation* cin = static_cast<jarvis::config::Operation*>(vectorOperationCIN[0]);
-        여기서부터 다시 작업 시작해야 합니다.
+        // LOG_DEBUG(logger, "Start applying Operation CIN");
+        // jarvis::config::Operation* cin = static_cast<jarvis::config::Operation*>(vectorOperationCIN[0]);
+        // 여기서부터 다시 작업 시작해야 합니다.
     }
 
     void applyRS485CIN(std::vector<jarvis::config::Base*>& vectorRS485CIN)
