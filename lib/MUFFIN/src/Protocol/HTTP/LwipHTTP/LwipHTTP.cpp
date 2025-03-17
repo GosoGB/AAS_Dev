3b725b3a23581a300bd921d07352251f06020183
Kim, Joo-Sung <165881661+joosung1995@users.noreply.github.com>
2025-03-17 15:24:42 +0900

    COIL, DISCRETE INPUT 영역 qty 처리 로직 추가, LWIP HTTP 지연에 따른 로직 변경 추가

--- lib/MUFFIN/src/Protocol/HTTP/LwipHTTP/LwipHTTP.cpp
+++ lib/MUFFIN/src/Protocol/HTTP/LwipHTTP/LwipHTTP.cpp
@@ -830,27 +830,43 @@ namespace muffin { namespace http {
             }
         }
 
-        while (client->available() > 0)
+        uint32_t lastReceiveTime = millis();
+        size_t receivedBytes = 0;
+        while (receivedBytes < mContentLength) 
         {
-            if (mFlags.test(static_cast<uint8_t>(flag_e::OCTET)) == false)
+            if (client->available() > 0) 
             {
-                if ((client->available() == 1) && (client->peek() == '0'))
+                lastReceiveTime = millis();
+
+                if (mFlags.test(static_cast<uint8_t>(flag_e::OCTET)) == false)
                 {
-                    client->read();
-                    file.write(0);
-                    break;
+                    if ((client->available() == 1) && (client->peek() == '0'))
+                    {   
+                        file.write(0);
+                        receivedBytes++;
+                        break;
+                    }
+                }
+                file.write(client->read());
+                receivedBytes++;
+            }
+            else
+            {
+                if (millis() - lastReceiveTime > SECOND_IN_MILLIS) 
+                {
+                    LOG_ERROR(logger,"TIMEOUT");
+                    file.close();
+                    return Status(Status::Code::BAD);
                 }
             }
-
-            file.write(client->read());
         }
+        
         file.write('\0');
         file.flush();
         file.close();
 
         return Status(Status::Code::GOOD);
     }
-
 
     LwipHTTP* lwipHTTP = nullptr;
 }}
